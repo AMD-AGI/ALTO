1c4e8012728983a73b327e6bb50edee751ab425d
alirezak <alireza.khodamoradi@amd.com>
2026-06-23 21:01:58 +0000

    Apply working LPT fixes on yann_adahop: bias-skip in swap_params, decomposed-linear device fix, gpt_oss_20b_lpt run config

--- alto/models/gpt_oss/config_registry.py
+++ alto/models/gpt_oss/config_registry.py
@@ -144,6 +144,20 @@ def gpt_oss_20b_pretrain() -> Trainer.Config:
 
 def gpt_oss_20b_lpt() -> Trainer.Config:
     config = gpt_oss_20b_pretrain()
+    config.training.global_batch_size = 16
+    config.parallelism.expert_tensor_parallel_degree = 1
+    config.parallelism.tensor_parallel_degree = 1
+    config.parallelism.expert_parallel_degree = 8
+    config.training.local_batch_size = 1
+    config.activation_checkpoint.mode = "none"
+    config.dataloader.dataset = "c4"
+    config.dataloader.dataset_path = None
+    config.validator.dataloader.dataset = "c4_validation"
+    config.validator.dataloader.dataset_path = None
+    config.checkpoint.enable = False
+    config.checkpoint.initial_load_in_hf = False
+    config.checkpoint.initial_load_in_hf_quantized = False
+    config.checkpoint.interval = 1000
     config.dump_folder = "gpt_oss_20b-pretrain-subset-mxfp4gemm_1d2d-hadamard-sr-lr4e-4-outputs"
     config.model_converters = ModelConvertersContainer.Config(converters=[
         ModelOptConverter.Config(recipe="./alto/models/gpt_oss/configs/lpt_recipe.yaml",),

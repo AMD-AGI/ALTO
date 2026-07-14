bf651ea9154da7baa96ea04c32bca775f34e50bc
Yue Sun <Yue.Sun2@amd.com>
2026-07-14 15:27:39 +0800

    feat: add mxfp8 (e4m3) flash-attention forward kernel

--- alto/kernels/mxfp8/__init__.py
+++ alto/kernels/mxfp8/__init__.py
@@ -1,3 +1,7 @@
 # Copyright (c) 2026 Advanced Micro Devices, Inc.
 #
 # SPDX-License-Identifier: MIT
+
+from .triton_flash_attention_mxfp8 import triton_attention_mxfp8
+
+__all__ = ("triton_attention_mxfp8", )

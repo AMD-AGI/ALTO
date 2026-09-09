91ddfe20d72f3fc44f438f0cd43b4aa589b2d022
Han Wang <Han.Wang@amd.com>
2026-09-09 03:13:32 +0000

    expr: mixed precision of fp4/fp8

--- alto/models/gpt_oss/config_registry.py
+++ alto/models/gpt_oss/config_registry.py
@@ -70,7 +70,7 @@ def gpt_oss_20b() -> Trainer.Config:
 def gpt_oss_20b_pretrain() -> Trainer.Config:
     config = gpt_oss_20b_orig()
     config.hf_assets_path = "/huggingface/hub/models--openai--gpt-oss-20b/snapshots/6cee5e81ee83917806bbde320786a8fb61efebee/"
-    config.dump_folder = "gpt_oss_20b-pretrain-subset-gbs32-bf16-std0.008-nobalance-lr8e-4-outputs"
+    config.dump_folder = "gpt_oss_20b-pretrain-subset-gbs32-bf16-std0.008all-nobalance-lr8e-4decay0.1-reducebf16-outputs"
     config.profiler.enable_profiling = False
     config.training.steps = 1200000
     config.training.local_batch_size = 1
@@ -79,7 +79,7 @@ def gpt_oss_20b_pretrain() -> Trainer.Config:
     config.optimizer.param_groups[0].optimizer_kwargs["lr"] = 8e-4
     config.optimizer.param_groups[0].optimizer_kwargs["betas"] = (0.9, 0.95)
     config.optimizer.param_groups[0].optimizer_kwargs["eps"] = 1e-5
-    config.lr_scheduler.min_lr_factor = 0.05
+    config.lr_scheduler.min_lr_factor = 0.1
     config.lr_scheduler.warmup_steps = 128
     config.lr_scheduler.decay_ratio = 1 - 128 / config.training.steps
     config.lr_scheduler.decay_type = "cosine"
@@ -100,12 +100,13 @@ def gpt_oss_20b_pretrain() -> Trainer.Config:
     config.validator.steps = 32
     config.activation_checkpoint = None
     config.debug.seed = 1234
+    config.debug.enable_structured_logging = False
     return config
 
 
 def gpt_oss_20b_lpt() -> Trainer.Config:
     config = gpt_oss_20b_pretrain()
-    config.dump_folder = "gpt_oss_20b-pretrain-subset-gbs32-mxfp4gemm_1d2d-hadamard-sr-uos-deosc_2000_200_4.0-std0.008-nobalance-lr8e-4-bitrefactor-outputs"
+    config.dump_folder = "gpt_oss_20b-pretrain-subset-gbs32-mxfp4gemm_1d2d-hadamard-sr-uos-deosc_1000_100_3.0-std0.008scaledvanilla-nobalance-lr8e-4-mxfp8qkvo12-bitrefactor-outputs"
     config.model_converters = ModelConvertersContainer.Config(converters=[
         ModelOptConverter.Config(recipe="./alto/models/gpt_oss/configs/lpt_recipe.yaml",),
     ],)

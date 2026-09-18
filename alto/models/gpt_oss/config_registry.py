f02ddf6ccc974220328e78a7dc10807871d575cc
Alireza Khodamoradi <alireza.khodamoradi@amd.com>
2026-09-18 17:14:07 +0000

    Update config registry

--- alto/models/gpt_oss/config_registry.py
+++ alto/models/gpt_oss/config_registry.py
@@ -51,6 +51,7 @@ __all__ = [
     "gpt_oss_20b_mxfp4_had_fixed_2dw_sr",
     "gpt_oss_20b_mxfp4_had_layer_2dw_sr",
     "gpt_oss_20b_mxfp4_3rht_2dw_sr",
+    "gpt_oss_20b_mxfp4_3rht_fixed_2dw_sr",
     "gpt_oss_20b_mxfp4_3rht_2dw_sr_uos",
     "gpt_oss_20b_mxfp4_3rht_1dw_sr"
 ]
@@ -185,8 +186,8 @@ def gpt_oss_20b_pretrain() -> Trainer.Config:
     config.optimizer.eps = 1e-5 # set by mlperf
     config.lr_scheduler.min_lr_factor = 0.1 # set by mlperf
     config.lr_scheduler.warmup_steps = 128 # can be edited for mlperf submission
-    #config.lr_scheduler.total_steps = 1200000
-    config.lr_scheduler.decay_ratio = 1 - 128 / config.training.steps #config.lr_scheduler.total_steps
+    config.lr_scheduler.total_steps = 1200000
+    config.lr_scheduler.decay_ratio = 1 - 128 / config.lr_scheduler.total_steps
     config.lr_scheduler.decay_type = "cosine"
     config.metrics.log_freq = 1
     config.metrics.enable_tensorboard = True
@@ -399,6 +400,16 @@ def gpt_oss_20b_mxfp4_3rht_2dw_sr() -> Trainer.Config:
     config.dump_folder = "gpt_oss_20b-pretrain-subset-mxfp4gemm_1d2d-hadamard-sr-lr4e-4-mxfp4-base"
     config.model_converters = ModelConvertersContainer.Config(converters=[
         ModelOptConverter.Config(recipe="./alto/models/gpt_oss/configs/mxfp4_3rht_2dw_sr.yaml",),
+    ],)
+    return config
+
+def gpt_oss_20b_mxfp4_3rht_fixed_2dw_sr() -> Trainer.Config:
+    """baseline MXFP4 quantization."""
+    config = gpt_oss_20b_lpt()
+    config.activation_checkpoint.mode = "none"
+    config.dump_folder = "gpt_oss_20b-pretrain-subset-mxfp4gemm_1d2d-hadamard-sr-lr4e-4-mxfp4-base"
+    config.model_converters = ModelConvertersContainer.Config(converters=[
+        ModelOptConverter.Config(recipe="./alto/models/gpt_oss/configs/mxfp4_3rht_fixed_2dw_sr.yaml",),
     ],)
     return config
 
